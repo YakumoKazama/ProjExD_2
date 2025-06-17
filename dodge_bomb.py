f89fd003ec7c05c1f81f2f77b336ed98cc657750
YakumoKazama <c0a2406442@edu.teu.ac.jp>
2025-06-17 14:34:26 +0900

    こうかとんと爆弾が画面外に出ないように

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,6 +14,20 @@ DELTA = { #押下キーと移動量を対応させる
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数: こうかとんRect or 爆弾Rect\n
+    戻り値: 横方向, 縦方向の真理値tuple（True:画面内 / False:画面外）\n
+    概要: Rectオブジェクトのleft, right, top, bottomの値から画面内・外を判定する\n
+    """
+    flag_x, flag_y = True, True
+
+    if (rct.left < 0) | (rct.right > WIDTH): flag_x = False #横
+    if (rct.top < 0) | (rct.bottom > HEIGHT): flag_y = False #縦
+
+    return (flag_x, flag_y)
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -27,7 +41,12 @@ def main():
     bb_radius = 10 #大きさ(半径)
     vx, vy = 5, 5 #速度
     bb_img = pg.Surface((2*bb_radius,2*bb_radius))
-    pg.draw.circle(bb_img, (255,0,0), (bb_radius, bb_radius), bb_radius)
+    pg.draw.circle(
+        bb_img, 
+        (255,0,0), 
+        (bb_radius, bb_radius), 
+        bb_radius
+    )
     bb_img.set_colorkey((0,0,0))
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT) #初期位置はランダム
@@ -41,6 +60,7 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
+        #こうかとんの移動量を押下キーに応じて決める
         key_lst = pg.key.get_pressed()
         sum_mv = [0,0]
 
@@ -51,9 +71,23 @@ def main():
 
         kk_rct.move_ip(sum_mv)
         bb_rct.move_ip(vx, vy)
+
+        #画面端判定
+        kk_bound = check_bound(kk_rct) #こうかとん. 画面外に出るとき, 移動前の位置に戻る
+        sum_mv_bound = [0, 0] #横, 縦
+        if not kk_bound[0]: sum_mv_bound[0] = -1 * sum_mv[0]
+        if not kk_bound[1]: sum_mv_bound[1] = -1 * sum_mv[1]
+        kk_rct.move_ip(sum_mv_bound)
+
+        bb_bound = check_bound(bb_rct) #爆弾. 画面端にぶつかると、バウンドする
+        if not bb_bound[0]: vx = -1 * vx
+        if not bb_bound[1]: vy = -1 * vy
+
+        #画面に表示
         screen.blit(kk_img, kk_rct)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
+
         tmr += 1
         clock.tick(50)
 
